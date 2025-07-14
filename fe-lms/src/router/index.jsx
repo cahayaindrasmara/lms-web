933e103749a67a968edaf79a9289cf189be93666
Cahaya Indrasmara <indrasmara.cahaya27@gmail.com>
2025-07-14 17:10:22 +0700

    create sign in lms

---
+++ fe-lms/src/router/index.jsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router-dom"
+import ManagerHome from "../pages/ManagerHome"
+import SignInPage from "../pages/SignIn"
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <ManagerHome />
+    },  {
+        path: "/manager/sign-in",
+        element: <SignInPage />
+    }
+])
+
+export default router;
